5df1ed4c76ff7d53e7e85e880bd03e689e2daaa4
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-06-07 18:36:05 +0200

    Create script.ps1

---
+++ BadUSB/PowerShellShit/script.ps1
@@ -0,0 +1,10 @@
+$count = 0
+
+Start-Sleep -Seconds 5
+
+while ($count -ne 10) {
+    [System.Windows.Forms.SendKeys]::SendWait("Fick dich @Yasin_fake#9005")
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+    $count = $count + 1
+}
+
