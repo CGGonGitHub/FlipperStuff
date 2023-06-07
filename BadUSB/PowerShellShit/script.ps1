a64dec89a4d1d21c373b8f221b8b92a205d17b51
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-06-07 18:57:37 +0200

    Update script.ps1

--- BadUSB/PowerShellShit/script.ps1
+++ BadUSB/PowerShellShit/script.ps1
@@ -1,9 +1,11 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
 $count = 0
 
 Start-Sleep -Seconds 5
 
 while ($count -ne 10) {
-    [System.Windows.Forms.SendKeys]::SendWait("Fick dich @Yasin_fake#9005")
+    [System.Windows.Forms.SendKeys]::SendWait("Fick dich @Yasin_fake#9005 ")
     [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
     $count = $count + 1
 }
