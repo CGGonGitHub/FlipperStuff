b02e7c0653e3f59cf4c93c7078dc012f41d62b88
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-06-15 08:44:27 +0200

    Update Time stuff.ps1

--- Time stuff.ps1
+++ Time stuff.ps1
@@ -1,4 +1,4 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
 $FormObject = [System.Windows.Forms.Form]
 $ButtonObject = [System.Windows.Forms.Button]
 $DropDownObject = [System.Windows.Forms.ComboBox]
@@ -6,34 +6,32 @@ $LabelObject = [System.Windows.Forms.Label]
 #Variables
 $Script:YearMode = 1
 
+$Year = (Get-Date).Year
+$Month = (Get-Date).Month
+$MonthName = (Get-Culture).DateTimeFormat.GetMonthName($Month)
+$DayOfYear = (Get-Date).DayOfYear
+$DayOfMonth = (Get-Date).Day
+$DayOfWeek = (Get-Date).DayOfWeek
+$DayName = (Get-Culture).DateTimeFormat.GetDayName($DayOfWeek)
+$Hour = (Get-Date).Hour
+$Minute = (Get-Date).Minute
+$Second = (Get-Date).Second
+$Millisecond = (Get-Date).Millisecond
+
+
 $Form = New-Object $FormObject
 $Form.Text = 'My first project that actually does something cool'
 $Form.ClientSize = '800, 800'
 $Form.FormBorderStyle = 'Fixed3D'
 $Form.BackColor = '#313131'
 
-#Year
-$YearButton = New-Object $LabelObject
-$YearButton.Location = New-Object System.Drawing.Point(700, 40)
-$YearButton.Text = '2023'
-$YearButton.ClientSize = '90, 45'
-$YearButton.Font = 'Comic Sans MS, 23'
-$YearButton.BackColor = 'transparent'
-$YearButton.Add_Click({OpenYearDetailed})
-
+#Left Side
 $DateLabel = New-Object $LabelObject
 $DateLabel.Location = New-Object System.Drawing.Point(40, 40)
-$Day = (Get-Date).Day
-$Month = (Get-Date).Month
-$DateLabel.Text = "$Day, $Month"
-$DateLabel 
+$DateLabel.Font = 'Comic Sans MS, 30, style=underline'
+$DateLabel.ClientSize = '300, 100'
+$DateLabel.Text = "$Day. $MonthName"
 
-$YearMonthsLabel = New-Object $LabelObject
-$YearMonthsLabel.Location = New-Object System.Drawing.Point(700, 80)
-$YearMonthsLabel.ClientSize = '120, 50'
-$YearMonthsLabel.Font = 'Comic Sans MS, 23'
-$YearMonthsLabel.Text = ''
-$YearMonthsLabel.BackColor = 'white'
 
 $Script:NumberLabel = New-Object $LabelObject
 $Script:NumberLabel.Location = New-Object System.Drawing.Point(230, 350)
@@ -42,7 +40,60 @@ $Script:NumberLabel.Font = 'Comic Sans MS, 50, style=Bold'
 $Script:NumberLabel.BackColor = 'transparent'
 $Script:NumberLabel.ClientSize = '400, 100'
 
+$finalNumber = (-join("$Hour",":","$Minute",":","$Second",":","$Millisecond"))
+$Script:NumberLabel.Text = $finalNumber
 
+# Right Side
+#Year
+$YearButton = New-Object $LabelObject
+$YearButton.Location = New-Object System.Drawing.Point(550, 40)
+$YearButton.Text = (-join("$Year", " Years"))
+$YearButton.ClientSize = '240, 45'
+$YearButton.Font = 'Comic Sans MS, 30'
+$YearButton.BackColor = 'white'
+$YearButton.Add_Click({OpenYearDetailed})
+
+$YearMonthsLabel = New-Object $LabelObject
+$YearMonthsLabel.Location = New-Object System.Drawing.Point(625, 90)
+$YearMonthsLabel.ClientSize = '165, 45'
+$YearMonthsLabel.Font = 'Comic Sans MS, 25'
+$YearMonthsLabel.Text = (-join("$Month", " Months"))
+$YearMonthsLabel.BackColor = 'white'
+
+$YearDaysLabel = New-Object $LabelObject
+$YearDaysLabel.Location = New-Object System.Drawing.Point(660, 140)
+$YearDaysLabel.ClientSize = '130, 45'
+$YearDaysLabel.Font = 'Comic Sans MS, 23'
+$YearDaysLabel.Text = (-join("$DayOfMonth", " Days"))
+$YearDaysLabel.BackColor = 'white'
+
+$YearHoursLabel = New-Object $LabelObject
+$YearHoursLabel.Location = New-Object System.Drawing.Point(675, 190)
+$YearHoursLabel.ClientSize = '115, 45'
+$YearHoursLabel.Font = 'Comic Sans MS, 20'
+$YearHoursLabel.Text = (-join("$Hour", " Hours"))
+$YearHoursLabel.BackColor = 'white'
+
+$YearMinutesLabel = New-Object $LabelObject
+$YearMinutesLabel.Location = New-Object System.Drawing.Point(675, 240)
+$YearMinutesLabel.ClientSize = '115, 35'
+$YearMinutesLabel.Font = 'Comic Sans MS, 15'
+$YearMinutesLabel.Text = (-join("$Minute", " Minutes"))
+$YearMinutesLabel.BackColor = 'white'
+
+$YearSecondsLabel = New-Object $LabelObject
+$YearSecondsLabel.Location = New-Object System.Drawing.Point(685, 280)
+$YearSecondsLabel.ClientSize = '105, 30'
+$YearSecondsLabel.Font = 'Comic Sans MS, 13'
+$YearSecondsLabel.Text = (-join("$Second", " Seconds"))
+$YearSecondsLabel.BackColor = 'white'
+
+$YearMilliSecondsLabel = New-Object $LabelObject
+$YearMilliSecondsLabel.Location = New-Object System.Drawing.Point(675, 315)
+$YearMilliSecondsLabel.ClientSize = '115, 30'
+$YearMilliSecondsLabel.Font = 'Comic Sans MS, 10'
+$YearMilliSecondsLabel.Text = (-join("$MilliSecond", " Milliseconds"))
+$YearMilliSecondsLabel.BackColor = 'white'
 
 
 function OpenYearDetailed {
@@ -66,7 +117,7 @@ function OpenYearDetailed {
 
 }
 
-$Form.Controls.AddRange(@($YearButton, $YearMonthsLabel, $DateLabel, $NumberLabel))
+$Form.Controls.AddRange(@($YearButton, $YearMonthsLabel, $DateLabel, $NumberLabel, $YearDaysLabel, $YearHoursLabel, $YearMinutesLabel, $YearSecondsLabel, $YearMilliSecondsLabel))
 $Form.ShowDialog()
 $Form.Dispose()
 
@@ -80,3 +131,8 @@ $Form.Dispose()
 #    $finalNumber = (-join("$Hour",":","$Minute",":","$Second",":","$Millisecond"))
 #    $Script:NumberLabel.Text = $finalNumber
 #}
+
+#Modes:
+#How far we are
+#Total of ever
+#Total of year
